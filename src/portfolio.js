af538170f9bb3bd6757d52ea941a43b48a73d846
ethan johnson <ethanprjohnson@gmail.com>
2021-02-20 00:53:24 -0500

    init

---
+++ src/portfolio.js
@@ -0,0 +1,323 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Summary And Greeting Section
+
+import emoji from 'react-easy-emoji';
+
+const illustration = {
+  animated: true, // set to false to use static SVG
+};
+
+const greeting = {
+  username: 'Eric Johnson',
+
+  title: "Hi all, I'm Eric",
+  subTitle: emoji(
+    'A passionate Business Analyst 🚀 Enrolled at Guelph University'
+  ),
+  resumeLink:
+    '',
+  displayGreeting: true, // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  linkedin: 'https://www.linkedin.com/in/eric-johnson-9538751b8/',
+  gmail: 'eric.p.johnson8@gmail.com',
+  // Instagram and Twitter are also supported in the links!
+  display: true, // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: 'What I do',
+  subTitle: 'Business Analyst student at Guelph University',
+  skills: [
+    emoji(
+      '⚡ Stocks'
+    ),
+    emoji('⚡ Charts'),
+    emoji(
+      '⚡ :)'
+    ),
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: 'Flip Burgers',
+      fontAwesomeClassname: 'fas fa-hamburger'
+    },
+    {
+      skillName: 'Microsoft Word',
+      fontAwesomeClassname: 'far fa-file-word',
+    },
+    {
+      skillName: 'Microsoft PowerPoint',
+      fontAwesomeClassname: 'far fa-file-powerpoint',
+    },
+    {
+      skillName: 'Microsoft Excel',
+      fontAwesomeClassname: 'far fa-file-excel',
+    },
+
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: 'Guelph University',
+      logo: require('./assets/images/guelph.jpg'),
+      subHeader: 'Business Analyst',
+      duration: 'September 2019 - April 2023',
+      desc: 'PLACEHOLDER',
+      descBullets: [
+        '3.8 GPA'
+      ],
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: 'Data Analyst', //Insert stack or technology you have experience in
+      progressPercentage: '100%', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Market Research',
+      progressPercentage: '100%',
+    },
+    {
+      Stack: 'Communication and Sales',
+      progressPercentage: '100%',
+    },
+  ],
+  displayCodersrank: false, // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+
+    {
+      role: 'Cook',
+      company: 'NameCamp 31',
+      companylogo: require('./assets/images/camp31.png'),
+      date: 'May 2019 – Oct 2020',
+      desc:
+        'flipping burgers'
+    },
+
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: '', // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: 'true', // Set true or false to show Contact profile using Github, defaults to true
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: 'Big Projects',
+  subtitle: 'SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH',
+  projects: [
+    {
+      image: require('./assets/images/saayaHealthLogo.webp'),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: 'Visit Website',
+          url:
+            'http://saayahealth.com/',
+        },
+        //  you can add extra buttons here.
+      ],
+    },
+    {
+      image: require('./assets/images/nextuLogo.webp'),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: 'Visit Website',
+          url:
+            'http://nextu.se/',
+        },
+      ],
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji('Achievements And Certifications 🏆 '),
+  subtitle:
+    'Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !',
+
+  achievementsCards: [
+    {
+      title: 'Google Code-In Finalist',
+      subtitle:
+        'First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.',
+      image: require('./assets/images/codeInLogo.webp'),
+      footerLink: [
+        {
+          name: 'Certification',
+          url:
+            'https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing',
+        },
+        {
+          name: 'Award Letter',
+          url:
+            'https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing',
+        },
+        {
+          name: 'Google Code-in Blog',
+          url:
+            'https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html',
+        },
+      ],
+    },
+    {
+      title: 'Google Assistant Action',
+      subtitle:
+        'Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.',
+      image: require('./assets/images/googleAssistantLogo.webp'),
+      footerLink: [
+        {
+          name: 'View Google Assistant Action',
+          url:
+            'https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en',
+        },
+      ],
+    },
+
+    {
+      title: 'PWA Web App Developer',
+      subtitle: 'Completed Certifcation from SMIT for PWA Web App Development',
+      image: require('./assets/images/pwaLogo.webp'),
+      footerLink: [
+        { name: 'Certification', url: '' },
+        {
+          name: 'Final Project',
+          url: 'https://pakistan-olx-1.firebaseapp.com/',
+        },
+      ],
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: 'Blogs',
+  subtitle:
+    'With Love for Developing cool stuff, I love to write and teach others what I have learnt.',
+
+  blogs: [
+    {
+      url:
+        'https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae',
+      title: 'Win a Google Assistant Tshirt and $200 in Google Cloud Credits',
+      description:
+        'Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?',
+    },
+    {
+      url: 'https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e',
+      title: 'Why REACT is The Best?',
+      description:
+        'React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.',
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: 'TALKS',
+  subtitle: emoji(
+    'I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅'
+  ),
+
+  talks: [
+    {
+      title: 'Build Actions For Google Assistant',
+      subtitle: 'Codelab at GDG DevFest Karachi 2019',
+      slides_url: 'https://bit.ly/saadpasta-slides',
+      event_url: 'https://www.facebook.com/events/2339906106275053/',
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji('Podcast 🎙️'),
+  subtitle: 'I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY',
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    'https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo',
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji('Contact Me ☎️'),
+  subtitle:
+    'Want to connect? Send me an email!',
+  emailAddress: 'eric.p.johnson8@gmail.com',
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: 'twitter', //Replace "twitter" with your twitter username without @
+  display: false, // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
